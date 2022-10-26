7b77be4ebd2d3847aaf32eb249e112848fcd4a23
BuildTools <unconfigured@null.spigotmc.org>
2022-10-26 18:08:47 -0400

    1

---
+++ ScavengeRUS/obj/Release/net6.0/linux-x64/PubTmp/Out/EFSQLScripts/ScavengeRUS.Services.ApplicationDbContext.sql
@@ -0,0 +1,56 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20221026213303_mig01')
+BEGIN
+    CREATE TABLE [Admin] (
+        [Id] int NOT NULL IDENTITY,
+        [Role] nvarchar(max) NULL,
+        [FirstName] nvarchar(max) NULL,
+        [LastName] nvarchar(max) NULL,
+        CONSTRAINT [PK_Admin] PRIMARY KEY ([Id])
+    );
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20221026213303_mig01')
+BEGIN
+    CREATE TABLE [Hunt] (
+        [Id] int NOT NULL IDENTITY,
+        [HuntUrl] nvarchar(max) NULL,
+        CONSTRAINT [PK_Hunt] PRIMARY KEY ([Id])
+    );
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20221026213303_mig01')
+BEGIN
+    CREATE TABLE [Players] (
+        [Id] int NOT NULL IDENTITY,
+        [FirstName] nvarchar(max) NULL,
+        [LastName] nvarchar(max) NULL,
+        CONSTRAINT [PK_Players] PRIMARY KEY ([Id])
+    );
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20221026213303_mig01')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20221026213303_mig01', N'6.0.10');
+END;
+GO
+
+COMMIT;
+GO
+
