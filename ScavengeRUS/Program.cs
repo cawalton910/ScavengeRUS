05dd4b462020933537b85538ae51f1db08c41f36
BuildTools <unconfigured@null.spigotmc.org>
2022-10-26 17:58:07 -0400

    1

--- ScavengeRUS/Program.cs
+++ ScavengeRUS/Program.cs
@@ -6,7 +6,7 @@ using ScavengeRUS.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddDbContext<ApplicationDbContext>(options => {
+builder.Services.AddDbContext<ApplicationDbContext>(options => { 
     options.UseSqlServer(
     builder.Configuration.GetConnectionString("DefaultConnection"));
 });
